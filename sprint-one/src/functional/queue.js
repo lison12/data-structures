4e543c90eca094c723ddc53fb56b256916ebb6e2
Wilson Li <wilsonli415@gmail.com>
2018-09-19 14:56:17 -0700

    Commit Queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,32 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var last = 0; // 2
+  var first = 0; // 0
+  //var size = last - first // 2
+
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[last] = value;
+    last ++
   };
 
   someInstance.dequeue = function() {
+    var firstRemove = storage[first]
+    delete storage[first]
+    first ++
+    return firstRemove
   };
 
   someInstance.size = function() {
+    //return size;
+    //if empty obj then return 0
+    // if (Object.keys(storage).length === 0) {
+    //   return 0
+    // } else {
+    return last - first > 0 ? last - first : 0
   };
 
   return someInstance;

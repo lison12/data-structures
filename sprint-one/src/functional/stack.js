4484c5dfe539de61eabdce4397d7a7d364173e36
Wilson Li <wilsonli415@gmail.com>
2018-09-19 12:26:41 -0700

    Commit stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,26 @@
 var Stack = function() {
   var someInstance = {};
+  //var array
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var count = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[count] = value
+    count++
   };
 
   someInstance.pop = function() {
+    var removed = storage[count - 1];
+    delete storage[count - 1];
+    count > 0 ? count-- : 0;
+    return removed
   };
 
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;

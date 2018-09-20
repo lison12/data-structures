d71e118ef9ab2a85dff2c34937273630c68f5ce5
Wilson Li <wilsonli415@gmail.com>
2018-09-19 21:11:39 -0700

    Commit Prototypal

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,30 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+var someInstances = Object.create(queueMethods);
+someInstances.storage = {};
+someInstances.first = 0;
+someInstances.last = 0;
+return someInstances;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  
+  enqueue: function(value) {
+    this.storage[this.last] = value;
+    this.last++;
+  },
+
+  dequeue: function() {
+    var remove = this.storage[this.first];
+    delete this.storage[this.first];
+    this.first++;
+    return remove;
+  },
+
+  size: function() {
+    return Object.keys(this.storage).length;
+  },
+};
 
 

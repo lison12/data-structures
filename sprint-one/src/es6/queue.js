697b20371e25d6093f6f2e7bf8b25b33818a5576
Wilson Li <wilsonli415@gmail.com>
2018-09-19 22:15:23 -0700

    Commit es6

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,21 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.first = 0;
+    this.last = 0;
   }
-
+  enqueue(value) {
+    this.storage[this.last] = value;
+    this.last++;
+  }
+  dequeue() {
+    var remove = this.storage[this.first];
+    delete this.storage[this.first];
+    this.first++;
+    return remove;
+  }
+  size() {
+    return Object.keys(this.storage).length;
+  }
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,22 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.last = 0;
+  }
+  push(value) {
+    this.storage[this.last] = value;
+    this.last++;
   }
 
+  pop() {
+    var remove = this.storage[this.last - 1];
+    delete this.storage[this.last - 1];
+    this.last--;
+    return remove;
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
+  }
 }

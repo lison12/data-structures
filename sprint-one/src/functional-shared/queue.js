581a096b65377c13b58a8726a96765786f64805c
Wilson Li <wilsonli415@gmail.com>
2018-09-19 20:42:00 -0700

    Commit Functional Shared

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,33 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.storage = {};
+  someInstance.first = 0;
+  someInstance.last = 0;
+  _.extend(someInstance, queueMethods)
+  return someInstance
 };
 
-var queueMethods = {};
+var queueMethods = {
+
+  enqueue: function(value) {
+    this.storage[this.last] = value
+    this.last ++
+  },
+
+  dequeue: function() {
+    var remove = this.storage[this.first]
+    delete this.storage[this.first]
+    this.first ++
+    return remove
+  },
+
+  size: function() {
+    var size = this.last - this.first;
+    return size > 0 ? size : 0;
+    //return size;
+  },
+};
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,35 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+var someInstance = {};
+  someInstance.last = 0;
+  someInstance.storage = {};
+  _.extend(someInstance, stackMethods)
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+
+  push: function(value) {
+    this.storage[this.last] = value;
+    this.last ++
+  },
+
+  pop: function() {
+    var remove = this.storage[this.last-1]
+    delete this.storage[this.last-1]
+    this.last --
+    return remove
+  },
+
+  size: function() {
+    return this.last > 0 ? this.last : 0
+  },
+};
+
+
+
+
+
 
 

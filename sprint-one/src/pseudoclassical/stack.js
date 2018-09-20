e3ccd7fb4d4e24e560c11cb034228e3da9d20639
Wilson Li <wilsonli415@gmail.com>
2018-09-19 21:55:25 -0700

    Commit Pseudoclassical

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,25 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+this.storage = {};
+this.last = 0;
 };
+
+Stack.prototype.push = function(value) {
+  this.storage[this.last] = value;
+  this.last++;
+},
+
+Stack.prototype.pop = function() {
+  var remove = this.storage[this.last - 1];
+  delete this.storage[this.last - 1];
+  this.last--;
+  return remove;
+},
+
+Stack.prototype.size = function() {
+  return Object.keys(this.storage).length;
+}
 
 
